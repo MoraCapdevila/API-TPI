1f78b65873854b6c46691cf61fa2e3eeb31c8939
Joaquin Gribodo <joacogribodo@gmail.com>
2023-05-24 23:39:47 -0300

    Modificaciones

--- EcommerceAPI/Controllers/ProductController.cs
+++ EcommerceAPI/Controllers/ProductController.cs
@@ -7,5 +7,40 @@ namespace EcommerceAPI.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly _productoService = new ProductoService();
+        [HttpGet("GetListado")]
+        public ActionResult<List<Producto>> GetListadoProductos()
+        {
+            var response = _productoService.ListarProductos();
+            return Ok(response);
+        }
+
+        [HttpGet("GetListado/{id}")]
+        public ActionResult<Producto> GetProductoPorId(int id)
+        {
+            var response = _productoService.ListarProductosPorId(id);
+            return Ok(response);
+        }
+
+        [HttpPost("PostProducto")]
+        public ActionResult<Producto> PostProducto([FromBody] Producto producto )
+        {
+            var response = _productoService.AgregarProducto(producto);
+            return Ok(response);
+        }
+
+        [HttpPut("PutProducto/{id}")]
+        public ActionResult<Producto> PutProducto(int id, [FromBody] Producto producto)
+        {
+            var response = _productoService.ActualizarProducto(id, producto);
+            return Ok(response);
+        }
+
+        [HttpDelete("DeleteProducto/{id}")]
+        public ActionResult<Producto> PutProducto(int id)
+        {
+            var response = _productoService.EliminarProducto(id);
+            return Ok(response);
+        }
     }
 }
